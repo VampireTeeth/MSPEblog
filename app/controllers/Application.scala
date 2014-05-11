e07819095233c9fb838c74b7681993b2f11864e7
VampireTeeth <vampireteeth001@gmail.com>
2014-05-11 14:30:56 +1000

    User page added

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,7 +3,13 @@ package controllers
 import play.api.mvc.{Action, Controller}
 
 object Application extends Controller {
-  def index = Action {
-    Ok(views.html.index("Hello World, Play Framework"))
-  }
+
+    def index = Action {
+        Ok(views.html.index("Hello World, Play Framework"))
+    }
+
+    def user() = Action { 
+        Ok(views.html.user("User Login"))
+    }
+  
 }
